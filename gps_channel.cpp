69332d9a286d3b4a11a4dd575c40fba7cdaf85ec
Damien Dusha <d.dusha@gmail.com>
2020-09-12 10:10:51 -0700

    Hide codegen in the channel constructor

---
+++ gps_channel.cpp
@@ -0,0 +1,67 @@
+// This file is part of:
+// gnss-sim: A GNSS Signal Simulator
+//
+// Copyright (c) 2020 Damien Dusha
+// SPDX-License-Identifier: MIT
+//
+// Derived from https://github.com/osqzss/gps-sdr-sim (MIT Licence):
+// Copyright (c) 2015-2020 Takuji Ebinuma
+
+#include "gps_channel.h"
+
+#include <cassert>
+
+namespace {
+
+/*! \brief generate the C/A code sequence for a given Satellite Vehicle PRN
+ *  \param[in] prn PRN nuber of the Satellite Vehicle
+ *  \param[out] ca Caller-allocated integer array of 1023 bytes
+ */
+std::array<int, CA_SEQ_LEN> GeneratePrnSequence(int prn)
+{
+    assert(prn >= 1 && prn <= 32);
+    std::array<int, CA_SEQ_LEN> ca;
+
+	constexpr std::array<int, 32> delay = {
+		  5,   6,   7,   8,  17,  18, 139, 140, 141, 251,
+		252, 254, 255, 256, 257, 258, 469, 470, 471, 472,
+		473, 474, 509, 512, 513, 514, 515, 516, 859, 860,
+		861, 862};
+	
+	int g1[CA_SEQ_LEN], g2[CA_SEQ_LEN];
+	int r1[N_DWRD_SBF], r2[N_DWRD_SBF];
+
+	for (int i = 0; i < N_DWRD_SBF; i++) {
+		r1[i] = r2[i] = -1;
+    }
+
+	for (int i = 0; i < CA_SEQ_LEN; i++)
+	{
+		g1[i] = r1[9];
+		g2[i] = r2[9];
+		int c1 = r1[2]*r1[9];
+		int c2 = r2[1]*r2[2]*r2[5]*r2[7]*r2[8]*r2[9];
+
+		for (int j = 9; j > 0; j--) 
+		{
+			r1[j] = r1[j-1];
+			r2[j] = r2[j-1];
+		}
+		r1[0] = c1;
+		r2[0] = c2;
+	}
+
+	for (int i = 0, j = CA_SEQ_LEN-delay[prn-1]; i < CA_SEQ_LEN; i++,j++) {
+		ca[i] = (1-g1[i]*g2[j%CA_SEQ_LEN])/2;
+    }
+
+	return ca;
+}
+
+}   // namespace
+
+GpsChannel::GpsChannel(int in_prn)
+    : prn(in_prn)
+    , code_sequence_(GeneratePrnSequence(in_prn))
+{
+}
---
+++ gps_channel.h
@@ -0,0 +1,56 @@
+// This file is part of:
+// gnss-sim: A GNSS Signal Simulator
+//
+// Copyright (c) 2020 Damien Dusha
+// SPDX-License-Identifier: MIT
+//
+// Derived from https://github.com/osqzss/gps-sdr-sim (MIT Licence):
+// Copyright (c) 2015-2020 Takuji Ebinuma
+
+#ifndef GPS_CHANNEL_H_
+#define GPS_CHANNEL_H_
+
+#include "gpssim.h"
+
+#include <array>
+
+/*! \brief Structure representing a Channel */
+class GpsChannel
+{
+  public:
+    GpsChannel() = default;
+    GpsChannel(int in_prn);
+
+    /// \brief PRN for the satellite.
+    int prn = 0;
+
+	double f_carr;                                /*< Carrier frequency */
+	double f_code;	/*< Code frequency */
+	double carr_phase;
+	double code_phase; /*< Code phase */
+	gpstime_t g0;	/*!< GPS time at start */
+	unsigned long sbf[5][N_DWRD_SBF]; /*!< current subframe */
+	unsigned long dwrd[N_DWRD]; /*!< Data words of sub-frame */
+	int initial_word;	/*!< initial word */
+	int initial_bit;	/*!< initial bit */
+	int initial_code;	/*!< initial code */
+	int dataBit;	/*!< current data bit */
+	int codeCA;	/*!< current C/A code */
+	AzimuthElevation azel;
+	range_t rho0;
+
+    int ComputeDataBit() const {
+        return (int)((dwrd[initial_word] >> (29 - initial_bit)) & 0x1UL)*2 - 1;
+    }
+
+    int ComputeCodeChip() const {
+        return code_sequence_[(int) code_phase]*2-1;
+    }
+
+  private:
+    /// \brief C/A code sequence.
+    std::array<int, CA_SEQ_LEN> code_sequence_;
+};
+
+#endif // GPS_CHANNEL_H_
+
